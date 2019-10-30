d2a88ab7dedb492a38f42f00f77d4aebacdd5b12
vanasnip <ivan.mamenemuno@gmail.com>
2019-10-30 04:57:49 +0000

    broke up original home page into different pieces, we also have a pages and components paradigm

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import sections from './directory.data';
+import './directory.styles.scss'
+import MenuItem from '../menu-item/menu-item.component';
+
+export default class Directory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sections
+    }
+  }
+  render() {
+    return <div className="directory-menu">
+      {
+        this.state.sections.map(section => (
+          <MenuItem title={section.title.toUpperCase()}></MenuItem>
+        ))
+      }
+    </div>
+  }
+}
