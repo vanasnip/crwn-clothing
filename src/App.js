bc487673c23189a8aab0506314ca9ce554abeefa
vanasnip <ivan.mamenemuno@gmail.com>
2019-11-03 09:12:50 +0000

    adding header comp and link to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component'
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 const NotFound = () => (
   <div className="not-found">404 not found</div>
 )
+const HatsComp = ({match:{params:{loc}}}) => (<div>{loc.toUpperCase()}</div>);
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
+        <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/shop/:loc" component={HatsComp}></Route>
         <Route exact path="/*" component={NotFound} />
       </Switch>
     </div>
